f86e5209f4b3a59438756b399ae8302d49b94812
Otannael <otannael.leal@etec.sp.gov.br>
2023-10-31 20:17:34 -0300

    Otannael

---
+++ programacao/vetores/vetores.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+	int lista[10];
+	int i;
+	//le os numeros do usuario e calcula o dobro
+	
+	for (i=0;i<10;i++){
+		printf("Digite um numero para a posicao %d:",i);
+		scanf("%d",&lista[i]);
+		lista[i] = lista[i] * 2; //calcula o dobro e guarda na mesma posicao
+				       
+	}
+
+	//Imprime o vetor com os valores dobrados
+	printf("Valores dobrados:\n");
+	for (i=0;i<10;i++) {
+		printf("Posicao %d: %d\n",i,lista[i]);
+	}
+	return 0;
+}
